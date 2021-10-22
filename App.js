136a935790689d0bf6d8097fce8296012b6ed057
Pkbworldwide <86311623+Pkbworldwide@users.noreply.github.com>
2021-10-22 20:44:02 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,58 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { LogBox } from "react-native";
+import ReduxThunk from "redux-thunk";
+import * as Notifications from "expo-notifications";
+
+import productReducer from "./store/reducers/product";
+import cartReducer from "./store/reducers/cart";
+import orderReducer from "./store/reducers/orders";
+import authReducer from "./store/reducers/auth";
+
+import AppNavigator from "./navigation/AppNavigator";
+import { API_URL, API_TOKEN } from "@env";
+
+
+fetch('${API_URL}/users', {
+  headers:{
+    'Authorization': 'Bearer {API_TOKEN}'
+  }
+})
+LogBox.ignoreAllLogs();
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+    };
+  },
+});
+
+const rootReducer = combineReducers({
+  products: productReducer,
+  cart: cartReducer,
+  order: orderReducer,
+  auth: authReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+export default function App() {
+  const [dataLoaded] = useFonts({
+    "open-sans-pro": require("./assets/fonts/SourceSansPro-Regular.ttf"),
+  });
+  if (!dataLoaded) {
+    <AppLoading />;
+  }
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+      <StatusBar style="auto" />
+    </Provider>
+  );
+}
